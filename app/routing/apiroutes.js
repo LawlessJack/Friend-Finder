896078fbc0f492ab3d0a434f8e2cbf5b5142dce6
Jack Lawless <Lawless.jackm@gmail.com>
2020-03-07 19:22:48 -0600

    touch up files, and friends api link working

--- app/routing/apiroutes.js
+++ app/routing/apiroutes.js
@@ -6,7 +6,7 @@ var friends = require('../data/friends.js');
 
 var bestmatch;
 module.exports = function (app) {
-	app.get('/friends', function (req, res) {
+	app.get('/api/friends', function (req, res) {
 		res.json(friends);
 	});
 
@@ -26,27 +26,21 @@ module.exports = function (app) {
 		var usrName = usrData.name;
 		var usrImage = usrData.image;
 		var usrScores = usrData.scores;
-
 		var totalDifference = 0;
 
-		//loop through the friends data array of objects to get each friends scores
-		// ?????
 		for (var i = 0; i < friends.length - 1; i++) {
 			totalDifference = 0;
 
-			//loop through that friends score and the users score and calculate the 
-			// absolute difference between the two and push that to the total difference variable set above
+			
 			for (var j = 0; j < 6; j++) {
-				// We calculate the difference between the scores and sum them into the totalDifference
+				
 				console.log(friends[i]);
 				totalDifference += Math.abs(parseInt(usrScores[j]) - parseInt(friends[i].scores[j]));
 				
-				// If the sum of differences is less then the differences of the current "best match"
 			}
 			console.log(totalDifference);
 			if (totalDifference <= Match.matchDifference) {
 
-				// Reset the bestMatch to be the new friend. 
 				Match.name = friends[i].name;
 				Match.photo = friends[i].photo;
 				Match.matchDifference = totalDifference;
--- server.js
+++ server.js
@@ -1,12 +1,9 @@
-// ==============================================================================
-// DEPENDENCIES
-
 var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
 // ==============================================================================
 // EXPRESS CONFIGURATION
-// This sets up the basic properties for our express server 
+
 
 var app = express(); // Tells node that we are creating an "express" server
 var PORT = process.env.PORT || 3000; // Sets an initial port. We'll use this later in our listener
